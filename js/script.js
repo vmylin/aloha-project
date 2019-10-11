912345f7370ca5d94d1e08f5fa202b23d43d1e30
Victor Lin <vlinmy06@gmail.com>
2019-10-10 19:03:36 -0700

    added fickity slider and js for email area

---
+++ js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function() { 
+console.log('hello world');
+//all of your code with go inside here
+
+// start of Flickity Slider
+const elem = document.querySelector('.main-carousel');
+const flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  wrapAround: true,
+  freeScroll: true
+}); //end of Flickity Slider
+
+// START OF UPDATES SECTION
+
+const emailForm = document.getElementsByClassName('email-form')[0];
+
+const emailBox = document.getElementsByClassName('email-area')[0];
+
+const submit = document.getElementsByClassName('sub-btn')[0];
+
+
+emailForm.addEventListener("submit", function(e) {
+  e.preventDefault();
+  
+  // console.log('form submitted');
+  const emailAdd = emailBox.value;
+  // console.log(emailAdd);
+  if (emailAdd === ""){
+    alert('Please enter your email address');
+  } else {
+    alert('Please wait for our future newsletters!');
+  }
+});
+
+
+// END OF UPDATES SECTION
+
+});// end of doc ready
+
